a3e897fe818c6e87a69edc2504decc47957c14b2
kamelbouyacoub <kamel_bouyacoub07@live.Fr>
2019-07-31 00:21:25 +0200

    exe2

--- ML/exe1/gradientDescentMulti.m
+++ ML/exe1/gradientDescentMulti.m
@@ -8,6 +8,8 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
+    
+    theta = theta - ((alpha/m)* (((X * theta) - y)' * X)');
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
