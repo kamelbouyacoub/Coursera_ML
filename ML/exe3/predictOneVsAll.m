85a2501df407a704efe5264cdef7a51d75819c8a
kamelbouyacoub <kamel_bouyacoub07@live.Fr>
2019-08-10 23:39:44 +0200

    one vs all prediction

--- ML/exe3/predictOneVsAll.m
+++ ML/exe3/predictOneVsAll.m
@@ -28,14 +28,13 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%  
+res =  zeros(size(X, 1), num_labels);  
+for c = 1:num_labels
+   res(:, c) = (sigmoid(X* all_theta(c,:)'));
+endfor
 
-
-
-
-
-
-
+[m p] = max(res, [], 2);
 % =========================================================================
 
 
