6d48359b8ee5039911cefa23616dc500a7a6a98e
kamelbouyacoub <kamel_bouyacoub07@live.Fr>
2019-08-10 17:30:35 +0200

    One vs All

--- ML/exe3/oneVsAll.m
+++ ML/exe3/oneVsAll.m
@@ -48,7 +48,14 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
-
+i = 1;
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+for c = 1:num_labels
+  initial_theta = zeros(size(X, 2), 1);
+  [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)),initial_theta, options);
+  all_theta(i,:) = theta'
+  i = i + 1;
+endfor
 
 
 
